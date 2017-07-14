72a41584aeb60ad4efbc93078f42c7159624236a
july77 <ljlpsy11@gmail.com>
2017-07-14 19:26:20 +0800

    test autoprefixer

---
+++ static/js/sign-in-up.js
@@ -0,0 +1,6 @@
+let $homepage = $('#homepage-success')
+$homepage.click(function(){
+  $('#homepage-success').css('display','none')
+  $('.sign-container').css('display','block')
+  $('#sign-in-form').css('display','block')
+})
